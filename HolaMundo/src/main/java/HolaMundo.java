7318d4be371d662410146a4ac69fa4df00ef1fef
albertoicg01 <albertoicg01@comunidad.unam.mx>
2021-11-24 19:38:45 -0600

    Se añade primer programa en Java

---
+++ HolaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author alber
+ */
+//La clase debe corresponder al nombre del archivo
+//En este caso HolaMundo.java
+public class HolaMundo{
+    //Agregamos metodo main
+    //El metodo no regresa ninguna informacion
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!");
+    }
+}
