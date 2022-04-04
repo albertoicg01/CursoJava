87b882ae181923dce595c6bb944614d433dd9fb5
Alberto Cruz <albertoicg01@comunidad.unam.mx>
2022-04-04 00:46:58 -0500

    A new project for doing more exercises is added

---
+++ Fundamentals/src/main/java/Fundamentals/Main.java
@@ -0,0 +1,11 @@
+package Fundamentals;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        var intNumber = 10;
+        
+        
+        System.out.println("The variable intNumber "
+                + "contains: "+intNumber);
+    }
+}
